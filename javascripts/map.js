b46737daea2e6fa148fe6a6bac822584b0525e8a
Mila Frerichs <mila.frerichs@gmail.com>
2015-02-18 12:35:22 +0100

    Automated commit at 2015-02-18 11:35:21 UTC by middleman-deploy 1.0.0

--- javascripts/map.js
+++ javascripts/map.js
@@ -28,9 +28,21 @@
   };
 
   highlightNearest = function(nearest) {
-    nearest.properties['marker-color'] = '#f00';
-    nearest.properties['fill'] = '#f00';
-    return nearest.properties['stroke'] = '#f00';
+    var highlightColor;
+    highlightColor = "#f00";
+    return featureLayer.eachLayer(function(layer) {
+      if (layer.feature === nearest) {
+        if (layer instanceof L.Marker) {
+          layer.setIcon(L.mapbox.marker.icon({
+            'stroke': highlightColor
+          }));
+        } else {
+          layer.setStyle({
+            color: highlightColor
+          });
+        }
+      }
+    });
   };
 
   createFeatureForNearestAndUserLocation = function(userLocation, nearest, layer) {
@@ -74,13 +86,31 @@
 
   map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([51.959, 7.626], 15);
 
-  featureLayer = L.mapbox.featureLayer().loadURL('http://parkleit-api.codeformuenster.org/').addTo(map).on('layeradd', function(e) {
-    return parkhaueser = this.getGeoJSON();
+  featureLayer = L.mapbox.featureLayer().loadURL('http://parkleit-api.codeformuenster.org/').addTo(map).on('ready', function(e) {
+    parkhaueser = this.getGeoJSON();
+    return featureLayer.eachLayer(function(layer) {
+      var html, statusColor;
+      html = layer.feature.properties.name + "<br/><p>Freie Plätze: " + layer.feature.properties.free;
+      layer.bindPopup(html);
+      statusColor = '#E83838';
+      if (layer.feature.properties.status === "frei") {
+        statusColor = '#6CBA5B';
+      }
+      if (layer instanceof L.Marker) {
+        layer.setIcon(L.mapbox.marker.icon({
+          'marker-color': statusColor
+        }));
+      } else {
+        layer.setStyle({
+          fillColor: statusColor
+        });
+      }
+    });
   });
 
   findMeLayer = L.mapbox.featureLayer().addTo(map);
 
-  nearestLayer = L.mapbox.featureLayer().addTo(map);
+  nearestLayer = L.mapbox.featureLayer();
 
   if (!navigator.geolocation) {
     geolocate.innerHTML = 'Geolocation is not available';
